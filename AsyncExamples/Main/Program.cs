019aed2be995321a0175bbd7d5faf7a73e3306c8
MrDallOca <tdalloca@gmail.com>
2019-10-17 10:24:52 -0300

    Added AsyncHelloWorld

--- AsyncExamples/Main/Program.cs
+++ AsyncExamples/Main/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Threading.Tasks;
 
-namespace Main
+namespace AsyncHelloWorld
 {
 	class Program
 	{
+		private const uint SEGUNDOS = 3;
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			MainAsync().Wait();
+
+			Console.WriteLine();
+
+			Task.Delay(TimeSpan.FromSeconds(SEGUNDOS)).Wait();
+			Console.WriteLine($"Sync Hello World depois de { SEGUNDOS } segundos!");
+		}
+
+		private async static Task MainAsync()
+		{
+			await Task.Delay(TimeSpan.FromSeconds(SEGUNDOS));
+			Console.WriteLine($"Async Hello World depois de { SEGUNDOS } segundos!");
 		}
 	}
 }
